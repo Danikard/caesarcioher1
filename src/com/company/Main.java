3b9af50b5beff8d66f8431a483ef5f5072f794b1
Danikard <kajothada@gmail.com>
2021-03-04 23:57:43 -0800

    add methods to main class

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,22 @@
 package com.company;
+import java.util.Scanner;
+import static com.company.encrypt.encrypt;
+import static com.company.decrypt.decrypt,
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        System.out.println("welcome to encrypt & decrypt");
+        System.out.println("enter Text");
+        System.out.println("enter Key");
+        Scanner cipher= new Scanner(System.in);
+        Scanner key =new Scanner(System.in);
+        String message=cipher.nextLine();
+        Integer n = key.nextInt();
+        String encryptText=encrypt(message,n);
+        String decryptText=decrypt(message, n);
+        System.out.println(encryptText);
+        System.out.println(decryptText);
     }
 }
